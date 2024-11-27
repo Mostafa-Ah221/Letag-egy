f2a288a5df86f79fcb66175e944c2278d2c723d2
Your Name <mostafaahmed1101997@gmail.com>
2024-11-27 10:58:45 +0200

    new up-27

--- src/components/Stock/Stock.jsx
+++ src/components/Stock/Stock.jsx
@@ -1,7 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import favicon from '../../../src/assets/images/favicon.png';
+import { ContextData } from '../../context/ContextApis';
+
 function Stock() {
-    const [towns, setTowns] = useState([]);
+    const [towns, setTowns] = useState([]); 
+    const {selectedTownId, setSelectedTownId } = useContext(ContextData);
+
     useEffect(() => {
         const fetchdata = async () => {
             try {
@@ -16,31 +21,52 @@ function Stock() {
         };
         fetchdata();
     }, []);
+
+    const handleTownChange = (event) => {
+        const selectedId = event.target.value;
+        setSelectedTownId(selectedId);
+        console.log(selectedId);
+    };
+
     return (
-        <>
-            <div className='container'>
-                <h1 className={`text-center font-bold text-3xl`}>يرجى اختيار المدينة الذي تريد التوصيل إليه</h1>
-                <select name='town' className='absolute right-0 w-96 h-6 my-16 w-96 mr-56 bg-[#a3a3a3] text-white'>
-                    {towns?.map((t) => (
-                        <>
-                            <option disabled className='text-black text-end' value={t.name}>{t.name}</option>
-                            {t?.regions.map((r) => (
-                                <>
-                                    <option className='text-white text-end' value={r.name}>{r.name}</option>
-                                </>
-                            ))}
-                        </>
-                    ))}
-                </select >
-                <div className={`mt-28`}>
-                    <Link to={"/home"}><button className={`bg-primary w-32 h-12 flex item-center justify-center absolute right-2 mr-52 text-white pt-3 hover:text-black hover:bg-white rounded-xl`}>اظهار المنتجات</button></Link>
-                </div>
-                <div className='absolute right-2 mr-52 mt-20'>
-                    <img src='../../../src/assets/images/favicon.png' alt="logo" />
-                </div>
+        <div className='container'>
+            <h1 className='text-center font-bold text-3xl' dir="rtl">
+                يرجى اختيار المدينة الذي تريد التوصيل إليه
+            </h1>
+            <select
+                name='town'
+                className='absolute right-0 w-96 h-10 my-16 mr-56 bg-[#a3a3a3] text-white'
+                value={selectedTownId} 
+                onChange={handleTownChange} 
+            >
+                <option value="" disabled>
+                    اختر المدينة
+                </option>
+                {towns?.map((t) => (
+                    <React.Fragment key={t.id}>
+                        <option disabled className='text-black text-end' value={t.id}>
+                            {t.name}
+                        </option>
+                        {t?.regions.map((r) => (
+                            <option key={r.id} className='text-white text-end' value={r.id}>
+                                {r.name}
+                            </option>
+                        ))}
+                    </React.Fragment>
+                ))}
+            </select>
+            <div className='mt-28'>
+                <Link to="/home">
+                    <button className='bg-primary w-32 h-12 flex items-center justify-center absolute right-2 mr-52 text-white pt-3 hover:text-black hover:bg-white rounded-xl'>
+                        اظهار المنتجات
+                    </button>
+                </Link>
             </div>
-        </>
-    )
+            <div className='absolute right-2 mr-52 mt-20'>
+                <img src={favicon} alt="logo" />
+            </div>
+        </div>
+    );
 }
 
-export default Stock
+export default Stock;
