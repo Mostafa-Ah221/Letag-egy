3cd99377f3a235caf2475cc791e7f286c7e3bbc9
Amr Gado <amrm.gado1261998@gmail.com>
2024-12-26 12:44:57 +0200

    Stock Icon Update

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRightFromBracket, faBuilding, faEarthAmericas, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
@@ -6,22 +6,43 @@ import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import { ContextData } from '../../context/ContextApis';
 import { CgProfile } from "react-icons/cg";
 import { FaRegHeart } from "react-icons/fa";
-import { useLanguage } from "../../context/LanguageContextPro"; 
+import { useLanguage } from "../../context/LanguageContextPro";
 import { useCart } from '../../context/CartContext';
+import { CgShutterstock } from "react-icons/cg";
 
 function Menu() {
-    let { userToken, setUserToken,userData } = useContext(ContextData);
+    let { userToken, setUserToken, userData } = useContext(ContextData);
     const { language, toggleLanguage } = useLanguage();
-  const {wishList } = useCart(); 
-
+    const { wishList } = useCart();
+    const [isStock, setIsStock] = useState(false);
+    useEffect(() => {
+        const fetchdata = async () => {
+            try {
+                const res = await fetch("https://tarshulah.com/api/domain/settings");
+                const resJson = await res.json();
+                const data = await resJson.data;
+                const stock = await data.multi_stocks_management;
+                if (stock == 1) {
+                    setIsStock(true);
+                }
+                else {
+                    setIsStock(false);
+                }
+                // console.log(isStock);
+            } catch (error) {
+                // console.log(error)
+            }
+        };
+        fetchdata();
+    }, []);
     return (
         <div className="hidden lg:flex bg-Neutral h-[95%] fixed z-50 top-4 flex-col items-center mr-4 ml-3">
             {/* Cart Icon */}
-           <div className="flex flex-col w-full h-14 items-center justify-center hover:cursor-pointer group duration-300 bg-primary ">
+            <div className="flex flex-col w-full h-14 items-center justify-center hover:cursor-pointer group duration-300 bg-primary ">
                 <div className="flex items-center justify-center text-xl">
                     <ShoppingCart />
                 </div>
-               </div>
+            </div>
 
             {/* Logout Icon */}
             <div className='flex flex-col items-center mt-24'>
@@ -42,7 +63,7 @@ function Menu() {
                             </div>
                         </NavLink>
                         <hr className="w-full border-neutral-400 relative bottom-1" />
-                        
+
                     </>
                 ) : (
                     <>
@@ -69,23 +90,34 @@ function Menu() {
                     </>
                 )}
                 <NavLink
-                            to={'/wishlist'}
-                            className="hover:cursor-pointer my-1 group duration-300 hover:bg-primary w-full p-2"
-                        >
-                            <div className="flex items-center justify-center relative">
-                                <FaRegHeart
-                                    icon={faRightFromBracket}
-                                    className="text-black text-[1.5rem] group-hover:text-white"
-                                />
-                                <div className="bg-primary flex justify-center items-center rounded text-center w-4 h-4 text-[0.8rem] text-white absolute bottom-[0.9rem] border border-white -right-1 shadow-md">
-                                <span>{wishList.length}</span>
-                                </div>
-                            </div>
-                            <div className='text-black text-right text-[0.8rem] group-hover:text-white'>
-                                {language === "ar" ? "المفضلة" : "Favorites"}
-                            </div>
-                        </NavLink>
-                        <hr className="w-full border-neutral-400 relative bottom-1" />
+                    to={'/wishlist'}
+                    className="hover:cursor-pointer my-1 group duration-300 hover:bg-primary w-full p-2"
+                >
+                    <div className="flex items-center justify-center relative">
+                        <FaRegHeart
+                            icon={faRightFromBracket}
+                            className="text-black text-[1.5rem] group-hover:text-white"
+                        />
+                        <div className="bg-primary flex justify-center items-center rounded text-center w-4 h-4 text-[0.8rem] text-white absolute bottom-[0.9rem] border border-white -right-1 shadow-md">
+                            <span>{wishList.length}</span>
+                        </div>
+                    </div>
+                    <div className='text-black text-right text-[0.8rem] group-hover:text-white'>
+                        {language === "ar" ? "المفضلة" : "Favorites"}
+                    </div>
+                </NavLink>
+                <hr className="w-full border-neutral-400 relative bottom-1" />
+                {isStock ? <>
+                    <NavLink
+                        to={'/'}
+                        className="hover:cursor-pointer my-1 group duration-300 hover:bg-primary w-full p-2 flex flex-col items-center justify-center"
+                    >
+                        <CgShutterstock className="text-black text-[1.5rem] group-hover:text-white" />
+                        <div className='text-black text-right text-[0.8rem] group-hover:text-white'>
+                            {language === "ar" ? "المخزن" : "Stock"}
+                        </div>
+                    </NavLink>
+                </> : ""}
             </div>
 
             {/* Main Icons Section */}
@@ -108,7 +140,7 @@ function Menu() {
                 {/* Language Icon */}
                 <div
                     className="mt-2 group hover:cursor-pointer hover:bg-primary w-full p-2"
-                    onClick={toggleLanguage} 
+                    onClick={toggleLanguage}
                 >
                     <div className="flex items-center justify-center">
                         <FontAwesomeIcon
@@ -125,30 +157,30 @@ function Menu() {
                 <hr className="w-full border-neutral-400" />
 
                 {/* Contact Us Icon */}
-                  <div className="mt-2 group hover:cursor-pointer hover:bg-primary p-1 mb-4 relative">
-                <div className="flex items-center justify-center">
-                    <FontAwesomeIcon
-                        icon={faPhone}
-                        className="text-black text-[1.2rem] group-hover:text-white"
-                    />
-                </div>
-                <p className="text-black text-center group-hover:text-white text-[0.8rem] mb-2">
-                    {language === "ar" ? "تواصل معنا" : "Contact Us"}
-                </p>
-                
-                {/* Phone Number Popup */}
-                <div className="absolute opacity-0 group-hover:opacity-100 bg-primary shadow-lg  py-4 px-2 right-full top-1/2 -translate-y-1/2 mr-1 transition-opacity duration-300 whitespace-nowrap">
-                    <div className="relative">
-                        {/* السهم */}
-                        {/* <div className="absolute w-2 h-2 bg-white rotate-45 -right-1 top-1/2 -translate-y-1/2"></div> */}
-                        
-                        {/* رقم الهاتف */}
-                        <p className=" text-sm text-white">
-                            {userData?.phone || "Not available"}
-                        </p>
+                <div className="mt-2 group hover:cursor-pointer hover:bg-primary p-1 mb-4 relative">
+                    <div className="flex items-center justify-center">
+                        <FontAwesomeIcon
+                            icon={faPhone}
+                            className="text-black text-[1.2rem] group-hover:text-white"
+                        />
+                    </div>
+                    <p className="text-black text-center group-hover:text-white text-[0.8rem] mb-2">
+                        {language === "ar" ? "تواصل معنا" : "Contact Us"}
+                    </p>
+
+                    {/* Phone Number Popup */}
+                    <div className="absolute opacity-0 group-hover:opacity-100 bg-primary shadow-lg  py-4 px-2 right-full top-1/2 -translate-y-1/2 mr-1 transition-opacity duration-300 whitespace-nowrap">
+                        <div className="relative">
+                            {/* السهم */}
+                            {/* <div className="absolute w-2 h-2 bg-white rotate-45 -right-1 top-1/2 -translate-y-1/2"></div> */}
+
+                            {/* رقم الهاتف */}
+                            <p className=" text-sm text-white">
+                                {userData?.phone || "Not available"}
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     );
