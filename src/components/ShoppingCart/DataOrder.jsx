84b1459d91efc9a7ef4f2fad1f77d048fe6e8927
Your Name <mostafaahmed1101997@gmail.com>
2024-12-29 16:40:49 +0200

    new up-29

--- src/components/ShoppingCart/DataOrder.jsx
+++ src/components/ShoppingCart/DataOrder.jsx
@@ -167,7 +167,7 @@ useEffect(() => {
           </div>
         </>
       )}
-    <table className="table-auto w-full border-collapse border border-gray-300">
+    <table className="table-auto w-full border-collapse border border-gray-300 text-xs sm:text-base">
       {/* Header Row */}
       <thead className="bg-gray-100">
         <tr>
