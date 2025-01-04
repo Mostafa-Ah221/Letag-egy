2e942b6285cfdc27cd602b4e0da9f6f728355410
Your Name <mostafaahmed1101997@gmail.com>
2025-01-04 18:50:21 +0200

    new up-04

--- src/context/ContextApis.jsx
+++ src/context/ContextApis.jsx
@@ -26,7 +26,7 @@ async function fetchProducts(filters, language) {
 }
 
 async function subCategories(language) {
-  const response = await axios.get(`https://demo.leetag.com/api/categories`, {
+  const response = await axios.get(`https://tarshulah.com/api/categories`, {
     headers: { lang: language },
   });
   return response.data;
@@ -65,7 +65,7 @@ async function getOffers(language) {
 
 async function getProdDetails(id, language) {
   const response = await axios.get(
-    `https://demo.leetag.com/api/product/show/${id}`,
+    `https://tarshulah.com/api/product/show/${id}`,
     {
       headers: { lang: language },
     }
@@ -94,14 +94,14 @@ async function getSlider(language) {
 }
 
 async function getCurrency(language) {
-  const response = await axios.get(`https://demo.leetag.com/api/domain/settings`, {
+  const response = await axios.get(`https://tarshulah.com/api/domain/settings`, {
     headers: { lang: language },
   });
 
   return response.data;
 }
 async function getMenuPage() {
-  const response = await axios.get(`https://demo.leetag.com/api/menu`);
+  const response = await axios.get(`https://tarshulah.com/api/menu`);
 
   return response.data;
 }
